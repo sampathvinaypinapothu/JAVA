767f74be4fdb7b356f7d41823c71d725cb52ee95
sampathvinaypinapothu <140889834+sampathvinaypinapothu@users.noreply.github.com>
2024-06-25 08:50:18 +0530

    Add files via upload

---
+++ Sample2.java
@@ -0,0 +1,55 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+class Sample2 extends JFrame implements ActionListener
+{
+    JTextField t1,t2,t3;
+    JButton b1,b2;
+    Sample2()
+    {
+        t1=new JTextField();
+        t2=new JTextField();
+        t3=new JTextField();
+        b1=new JButton("+");
+        b2=new JButton("-");
+        t1.setBounds(100,150,100,60);
+        t2.setBounds(100,100,100,60);
+        t3.setBounds(100,50,100,60);
+        b1.setBounds(100,30,50,20);
+        b2.setBounds(150,30,50,20);
+        add(t1);
+        add(t2);
+        add(t3);
+        add(b1);
+        add(b2);
+        setTitle("addition");
+        setSize(500,600);
+        b1.addActionListener(this); //regestering the component with the listener
+        b2.addActionListener(this);
+        setLayout(null);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+    //interface implementation
+    public void actionPerformed(ActionEvent e)
+    {
+        String s1=t1.getText();
+        String s2=t2.getText();
+        int a=Integer.parseInt(s1);
+        int b=Integer.parseInt(s2);
+        int c=0;
+        if(e.getSource()==b1) 
+        {
+            c=a+b;
+        }
+        else 
+        {
+            c=a-b;
+        }
+        String result=String.valueOf(c);
+        t3.setText(result);
+    }
+     public static void main(String[] args) {
+        new Sample2();
+     }
+}
